907ae74295aa06ba008fcf99bcd21401e27f3517
nikita <bigmama93@mail.ru>
2022-11-22 15:34:56 +0300

    edit create_intent(bot answers all the questions)

--- create_intent.py
+++ create_intent.py
@@ -10,24 +10,27 @@ def create_intent(project_id):
     training_phrases_parts = json.loads(phrases)
     intents_client = dialogflow.IntentsClient()
     parent = dialogflow.AgentsClient.agent_path(project_id)
-    training_phrases = []
-    response_messages = []
+
+
     for training_phrases_part, phrase_item in training_phrases_parts.items():
+        training_phrases = []
+        response_messages = []
         for item in phrase_item['questions']:
+
             part = dialogflow.Intent.TrainingPhrase.Part(text=item)
             training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
             training_phrases.append(training_phrase)
         text = dialogflow.Intent.Message.Text(text=[phrase_item['answer']])
         message = dialogflow.Intent.Message(text=text)
         response_messages.append(message)
-    display_name = 'Как устроиться к вам на работу'
-    intent = dialogflow.Intent(
-        display_name=display_name, training_phrases=training_phrases, messages=response_messages
-    )
-    response = intents_client.create_intent(
-        request={"parent": parent, "intent": intent}
-    )
-    print("Intent created: {}".format(response))
+        display_name = f'{training_phrases_part}'
+        intent = dialogflow.Intent(
+            display_name=display_name, training_phrases=training_phrases, messages=response_messages
+        )
+        response = intents_client.create_intent(
+            request={"parent": parent, "intent": intent}
+        )
+        print("Intent created: {}".format(response))
 
 
 if __name__ == '__main__':
